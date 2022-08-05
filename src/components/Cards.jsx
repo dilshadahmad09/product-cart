2d86757ca5808bfd70fd3ad5aa9a7a1c9b307bc0
Dilshad <dilshadsoraon09@gmail.com>
2022-08-05 22:45:33 +0530

    let's start again and complete search filteration and dropdown part

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,31 +6,53 @@ import Cardsdata from './CardsData';
 import './style.css';
 import { ADD } from '../redux/actions/action';
 import { useDispatch } from 'react-redux';
+import Dropdown from "react-bootstrap/Dropdown";
 const Cards = () => {
     const [data, setData] = useState(Cardsdata);
     const dispatch = useDispatch();
     const sendData = (e)=>{
         dispatch(ADD(e))
     }
-    
-    const filter = ()=>{}
+     console.log(data);
+    const filter = ()=>{
+        setData(data.sort((a,b)=>{
+            return b.rating - a.rating;
+        }))
+       
+        console.log("dilshad",data)
+    }
+
   return (
     <div className='container mt-3'>
       <h1 className='text-center'>Add to Carts Project</h1>
       <div className='search-filter-dropdown'>
-        <div className='dropdown'>DropDown</div>
+        <div className='dropdown'>
+          <Dropdown>
+            <Dropdown.Toggle variant='success' id='dropdown-basic'>
+              Category
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href='#/action-1'>Dinner</Dropdown.Item>
+              <Dropdown.Item href='#/action-2'>Lunch</Dropdown.Item>
+              <Dropdown.Item href='#/action-3'>Break Fast</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
         <div className='search'>
           <input type='text' placeholder='search by name or category ...' />
-            <i className='fas fa-search'></i>
+          <i className='fas fa-search'></i>
         </div>
-        <div className='filter'>Filter by price</div>
+        <div className='filter'>
+          <button onClick={filter}>Filter By Price</button>
+        </div>
       </div>
       <div className='row d-flex justify-content-center align-items-center'>
-        {data.map((element, id) => {
+        {data.map((element) => {
           return (
             <>
               <Card
-                key={id}
+                key={element.id}
                 style={{ width: "18rem", border: "none", textAlign: "left" }}
                 className='mx-2 mt-4 card_style'
               >
@@ -46,6 +68,7 @@ const Cards = () => {
                     Some quick example text to build on the card title and make
                     up the bulk of the card's content.
                   </Card.Text>
+                  <Card.Text>Rating : ₹ {element.rating}</Card.Text>
                   <Card.Text>Price : ₹ {element.price}</Card.Text>
                   <div className='button_div d-flex justify-content-center align-items-center'>
                     <Button
