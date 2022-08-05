f40999012351674552e05aec48155e3ff00796aa
Dilshad <dilshadsoraon09@gmail.com>
2022-08-05 10:52:08 +0530

    cards component is completed with manual data and building some dummy data for displaying in cardData.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
 import {Routes, Route}  from 'react-router-dom';
+import Cards from "./components/Cards";
+import CardDetails from "./components/CardDetails";
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Routes>
+        <Route path='/' element={<Cards />} />
+        <Route path='/cart' element={<CardDetails />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,89 @@
-import React from "react";
+import React,{useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Badge from "@mui/material/Badge";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import { NavLink } from "react-router-dom";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import './style.css'
 
 const Header = () => {
+
+      const [anchorEl, setAnchorEl] = useState(null);
+      const open = Boolean(anchorEl);
+      const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+      };
+      const handleClose = () => {
+        setAnchorEl(null);
+      };
   return (
-    <Navbar bg='dark' variant='dark'>
+    <Navbar bg='dark' variant='dark' style={{ height: "60px" }}>
       <Container>
-        <Navbar.Brand href='#home'>Add to Cart</Navbar.Brand>
+        <NavLink to='/' className='text-decoration-none text-light mx-3'>
+          Add to Cart
+        </NavLink>
         <Nav className='me-auto'>
-          <Nav.Link href='#home'>Home</Nav.Link>
+          <NavLink to='/' className='text-decoration-none text-light'>
+            Home
+          </NavLink>
         </Nav>
-        <Badge badgeContent={4} color='success'>
+        <Badge
+          badgeContent={4}
+          color='success'
+          id='demo-positioned-button'
+          aria-controls={open ? "demo-positioned-menu" : undefined}
+          aria-haspopup='true'
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
           <i
             class='fa-solid fa-cart-shopping text-light'
             style={{ fontSize: 25, cursor: "pointer" }}
           ></i>
         </Badge>
       </Container>
+      <Menu
+        id='demo-positioned-menu'
+        aria-labelledby='demo-positioned-button'
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <div
+          className='card_details d-flex justify-content-center align-items-center'
+          style={{ width: "24rem", padding: 10, position: "relative" }}
+        >
+          <i
+            className='fas fa-close smallclose'
+            style={{
+              position: "absolute",
+              right: 20,
+              top: 2,
+              fontSize: 23,
+              cursor: "pointer",
+            }}
+            onClick={handleClose}
+          ></i>
+          <p style={{ fontSize: 22 }}>Your Cart is Empty!</p>
+          <img
+            className='emptycart_img'
+            style={{ width: "5rem", padding: 10 }}
+            src='https://www.pngkey.com/png/detail/231-2317260_an-empty-shopping-cart-viewed-from-the-side.png'
+            alt=''
+          />
+        </div>
+      </Menu>
     </Navbar>
   );
 };
